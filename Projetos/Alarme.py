cce494524a57167ef62408106e48d66c55801ead
Saulo Borges <saulobdsena@gmail.com>
2025-04-26 13:11:26 -0300

    Up

--- Projetos/Alarme.py
+++ Projetos/Alarme.py
@@ -1,11 +1,41 @@
 import tkinter as tk
 from tkinter import Label, Frame
+import datetime
+import time
+import threading
+from playsound import playsound
 
+def alarme():
+    setTempoAlarme = f'{hora.get()}:{minutos.get()}:{segundo.get()}'
+    while True:
+        time.sleep(1)
+        horaLocal = datetime.datetime.now().strftime('%H:%M:%S')
+        print(horaLocal, setTempoAlarme)
+        if horaLocal == setTempoAlarme:
+            playsound(r'C:\Users\saulo\Documents\GitHub\Python\Projetos\Sounds\somDespertador.mp3')
+            break
 
 janela = tk.Tk()
-
-tk.Label(frame, text='Alarme Pythônico', pady=40).grid(column=1, row=1, columnspan=2)
-frame = Frame(janela, padx=40, pady=40).grid(column=1, row=1)
-
+janela.geometry("400x200")
 janela.title('Alarme Python')
-janela.mainloop
+
+tk.Label(janela, text='Alarme', font=('Helvetica', 20, 'bold')).pack(pady=10)
+tk.Label(janela, text='Definir Alarme').pack(pady=5)
+
+frame = tk.Frame(janela)
+frame.pack()
+
+def opcoes(value):
+    opc = tk.StringVar(janela)
+    opcoes = [str(i).zfill(2) for i in range(value)]
+    opc.set(opcoes[0])
+    tk.OptionMenu(frame, opc, *opcoes).pack(side=tk.LEFT)
+    return opc
+
+hora = opcoes(24)
+minutos = opcoes(60)
+segundo = opcoes(60)
+
+tk.Button(janela, text='Definir', font=('Helvetica', 15), command=lambda: threading.Thread(target=alarme).start()).pack(pady=20)
+
+janela.mainloop()
